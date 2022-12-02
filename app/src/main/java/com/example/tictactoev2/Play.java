582a3b25fe8d2473840b7688509a4cdca0ec62ab
Joseph Bombardier <joseph@jopem.com>
2022-12-02 14:50:53 -0500

    X

--- app/src/main/java/com/example/tictactoev2/Play.java
+++ app/src/main/java/com/example/tictactoev2/Play.java
@@ -1,5 +1,8 @@
 package com.example.tictactoev2;
 
+import static java.lang.Math.floor;
+import static java.lang.Math.sqrt;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,8 +10,12 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.jar.Attributes;
+
 public class Play extends AppCompatActivity {
     int QuiJou = 1; // 1 ou -1
+    String[] Names = new String[] {"Player 1", "Player 2"};
+    String[] symbole = new String[] {"X", "Y"};
     TextView comment = (TextView) findViewById(R.id.GameComment);
 
     @Override
@@ -24,11 +31,29 @@ public class Play extends AppCompatActivity {
             public void onClick(View view) {
                 ClickBtn(btn11);
             }
+        }));/*
+        Button btn12 = (Button) findViewById(R.id.button12);
+        btn12.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickBtn(btn12);
+            }
         }));
+        Button btn13 = (Button) findViewById(R.id.button13);
+        btn13.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickBtn(btn13);
+            }
+        }));*/
+
+
     }
     public void ClickBtn(Button btn){
         if (btn.getText() == "") {
-
+            int temp = 0;
+            if (QuiJou == -1) temp = 1;
+            btn.setText(symbole[temp]);
         }
         else comment.setText("This square is already taken");
 
