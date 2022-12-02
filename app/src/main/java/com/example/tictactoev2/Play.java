fef279443c562d6f6987baaef3c4b45f14ab35b6
Joseph Bombardier <joseph@jopem.com>
2022-12-02 14:20:38 -0500

    viell vertion

---
+++ app/src/main/java/com/example/tictactoev2/Play.java
@@ -0,0 +1,26 @@
+package com.example.tictactoev2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Play extends AppCompatActivity {
+    int QuiJou = 1; // 1 ou -1
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+
+        Button btn11 = (Button) findViewById(R.id.button11);
+        btn11.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //EditText editText = (EditText)findViewById(R.id.button11);
+                //editText.setText("Username");
+            }
+        }));
+    }
+}
