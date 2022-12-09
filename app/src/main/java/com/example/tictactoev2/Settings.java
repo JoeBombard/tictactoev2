182bbd5d3472e4f874a474eb26d8684b78de5869
Joseph Bombardier <joseph@jopem.com>
2022-12-09 14:51:35 -0500

    yes on switch

--- app/src/main/java/com/example/tictactoev2/Settings.java
+++ app/src/main/java/com/example/tictactoev2/Settings.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -18,7 +19,7 @@ public class Settings extends AppCompatActivity {
     String player1;
     String player2;
     Button confirmButton;
-    TextView comment;
+    TextView comment, name1, name2;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -27,7 +28,18 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         confirmButton = findViewById(R.id.ButComfirmName);
-
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ClickConfirm();
+            }
+        });
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        if ((sharedPref.getString("player1name", "")) != "" && (sharedPref.getString("player2name", "")) != "")
+        {
+            name1.setText(sharedPref.getString("player1name", ""));
+            name2.setText(sharedPref.getString("player2name", ""));
+        }
 
         comment = (TextView) findViewById(R.id.textViewWhoStart);
 
