a9d7b08d965e8acc6f07cdf58d87831f0340c14d
simtail <simtailbeast99@gmail.com>
2022-12-09 14:39:52 -0500

    Changes

---
+++ app/src/main/java/com/example/tictactoev2/ScoreHistory.java
@@ -0,0 +1,14 @@
+package com.example.tictactoev2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScoreHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_history);
+    }
+}
